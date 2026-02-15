5800e8db89e33f02a62bf2d5b1d6788311de4960
Abdul Rafay <abdulrafayaptech32@gmail.com>
2026-02-15 12:52:13 +0500

    Blog Section Added AND App Section

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ window.addEventListener("scroll", () => {
     nav.classList.remove("scrolled");
   }
 });
+const counters = document.querySelectorAll('.counter');
+const speed = 200; // Speed jitna kam hoga animation utna fast hoga
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+
+        const inc = target / speed;
+
+        if (count < target) {
+            counter.innerText = Math.ceil(count + inc);
+            setTimeout(updateCount, 10);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    // Intersection Observer to start animation when visible
+    const observer = new IntersectionObserver((entries) => {
+        if(entries[0].isIntersecting){
+            updateCount();
+        }
+    }, { threshold: 0.5 });
+
+    observer.observe(counter);
+});
